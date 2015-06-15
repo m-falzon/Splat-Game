a2f6bf4943b81cbb812a8fc85fd51dc72f98ec0e
Matthew Falzon <m.falzon@live.com>
2015-06-15 20:29:41 +1000

    Started adding random colour generator

---
+++ Splat Game/Splat/HomeController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeController.swift
+//  Splat
+//
+//  Created by Matthew Falzon on 14/06/2015.
+//  Copyright (c) 2015 Matthew Falzon. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
--- Splat Game/Splat/ViewController.swift
+++ Splat Game/Splat/ViewController.swift
@@ -11,56 +11,51 @@ import SpriteKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var ball: UIImageView!
+    
     @IBOutlet weak var number: UILabel!
     @IBOutlet weak var result: UILabel!
     
-    @IBOutlet weak var redColour: UIImageView!
-    @IBOutlet weak var blueColour: UIImageView!
     
-    func randomNumberFunc() -> Int {
-        return Int(arc4random_uniform(3))
+    func randomizeColor() {
+        
+        var red = UIColor(red: (221/255.0), green: (0/255.0), blue: (37/255.0), alpha: 1.0)
+        var blue = UIColor(red: (160/255.0), green: (97/255.0), blue: (5/255.0), alpha: 1.0)
+        
+        var colours = [red, blue]
+        
+        ball.tintColor = blue
+        
     }
-    
-//    var randomNumber = Int(arc4random_uniform(2)+1)
-    
-    var randomColour = ["Red", "Blue"]
+
+    func randomNumberFunc() -> Int {
+        return Int(arc4random_uniform(2)+1)
+    }
 
     override func viewDidLoad() {
         
         super.viewDidLoad()
-    
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
-    
-//    @IBAction func swipe(sender: UISwipeGestureRecognizer) {
-//        
-//        switch sender.direction {
-//        case UISwipeGestureRecognizerDirection.Up:
-//            println("Swiped up")
-//        case UISwipeGestureRecognizerDirection.Down:
-//            println("Swiped down")
-//        default:
-//            break
-//        }
-//    }
-    
-    @IBAction func startButton(sender: AnyObject) {
+        
+        
+        // Timer
+        
+        var timer = NSTimer.scheduledTimerWithTimeInterval(3, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
         
         number.text = "\(randomNumberFunc())"
+
         
-        //------------right  swipe gestures in view--------------//
-        let swipeRight = UISwipeGestureRecognizer(target: self, action: Selector("rightSwiped"))
-        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
-        self.view.addGestureRecognizer(swipeRight)
         
-        //-----------left swipe gestures in view--------------//
-        let swipeLeft = UISwipeGestureRecognizer(target: self, action: Selector("leftSwiped"))
-        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
-        self.view.addGestureRecognizer(swipeLeft)
+        // MARK: Swipe Gestures
+        
+//        //------------right  swipe gestures in view--------------//
+//        let swipeRight = UISwipeGestureRecognizer(target: self, action: Selector("rightSwiped"))
+//        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+//        self.view.addGestureRecognizer(swipeRight)
+//        
+//        //-----------left swipe gestures in view--------------//
+//        let swipeLeft = UISwipeGestureRecognizer(target: self, action: Selector("leftSwiped"))
+//        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+//        self.view.addGestureRecognizer(swipeLeft)
         
         //-----------down swipe gestures in view--------------//
         let swipeDown = UISwipeGestureRecognizer(target: self, action: Selector("downSwiped"))
@@ -72,38 +67,51 @@ class ViewController: UIViewController {
         swipeUp.direction = UISwipeGestureRecognizerDirection.Up
         self.view.addGestureRecognizer(swipeUp)
         
-        var timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
-    }
-    
-    func update() {
-        number.text = String(randomNumberFunc())
-    }
-    
-    func rightSwiped() {
-        println("right swiped")
-    }
-    
-    func leftSwiped() {
-        println("left swiped")
-    }
-    
-    func downSwiped() {
-        if randomNumberFunc() == 2 {
-            result.text = "True"
-        } else {
-            result.text = "False"
         }
-    }
     
-    func upSwiped() {
-        if randomNumberFunc() == 1 {
-            number.text = "\(randomNumberFunc())"
-        } else {
-            result.text = "False"
+    
+        // Update Function
+    
+        func update() {
+            randomNumberFunc()
         }
-    }
     
+    
+        // Swipe Functions
+    
+//        func rightSwiped() {
+//            println("right swiped")
+//        }
+//    
+//        func leftSwiped() {
+//            println("left swiped")
+//        }
+    
+        func downSwiped() {
+            if randomNumberFunc() == 2 {
+                result.text = "True"
+                println("swipe down")
+            } else {
+                result.text = "False"
+            }
+        }
+    
+        func upSwiped() {
+            if randomNumberFunc() == 1 {
+                result.text = "True"
+                println("swipe up")
+            } else {
+                result.text = "False"
+            }
+        }
+    
+    
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
     
 }
-
-
